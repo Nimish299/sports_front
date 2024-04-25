956ed024192cc53f52f2cf1ad2d8bdf7aff4ac63
Nimish299 <f20201857@hyderabad.bits-pilani.ac.in>
2024-04-25 16:57:26 +0530

    headers proxy remove 2

--- src/pages/player/PlayerHome.js
+++ src/pages/player/PlayerHome.js
@@ -22,7 +22,7 @@ const PlayerHome = () => {
   };
 
   const gotoPlayerPlayer = () => {
-    return navigate('https://sports-back.onrender.com/player/playerplayer');
+    return navigate('player/playerplayer');
   };
   const { loginflag, setLoginflag } = FlagState();
 
